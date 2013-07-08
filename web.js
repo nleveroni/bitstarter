3c98e24f519d6dccec7c1d162cd8357bcd2a1c01
nicolas leveroni <nicolas.leveroni@gmail.com>
2013-07-08 13:53:54 +0000

    Added step 7 try3

--- web.js
+++ web.js
@@ -7,8 +7,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+  var mybuffer = fs.readFileSync("index.html");
+  response.send(buffer.toString('utc 8', mybuffer));
 });
 
 
